bfe91cb9b98ba7f894abefdb64ffa47755dc9cf0
Bartek <bartraw23@gmail.com>
2020-10-23 15:23:10 +0200

    Add Translator pojo

---
+++ postgresql/src/main/java/io/liski/postgresql/domain/Translator.java
@@ -0,0 +1,46 @@
+package io.liski.postgresql.domain;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Translator {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(cascade = {CascadeType.ALL})
+    @JoinTable(
+            name = "Translator_Book",
+            joinColumns = {@JoinColumn(name = "translator_id")},
+            inverseJoinColumns = {@JoinColumn(name = "book_id")}
+    )
+    Set<Book> books = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
