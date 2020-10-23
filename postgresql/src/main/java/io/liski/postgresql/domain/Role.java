1a74819a0fc574bd452ee4ebe9f628b81ed79170
Bartek <bartraw23@gmail.com>
2020-10-23 16:09:01 +0200

    Add Role pojo

---
+++ postgresql/src/main/java/io/liski/postgresql/domain/Role.java
@@ -0,0 +1,41 @@
+package io.liski.postgresql.domain;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy="role")
+    Set<User> users = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
