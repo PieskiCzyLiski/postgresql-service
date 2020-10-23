2dce1ddfa37a335e396736ff95bb94f6a393933e
Bartek <bartraw23@gmail.com>
2020-10-23 16:09:23 +0200

    Add Reservation pojo

---
+++ postgresql/src/main/java/io/liski/postgresql/domain/Reservation.java
@@ -0,0 +1,68 @@
+package io.liski.postgresql.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    @OneToOne(mappedBy = "reservation")
+    private User user;
+
+    @OneToOne(mappedBy = "reservation")
+    private Book book;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+}
