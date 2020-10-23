728db6523e4cfd485e86b522ef7292df0e864877
Bartek <bartraw23@gmail.com>
2020-10-23 15:25:07 +0200

    Add Book pojo

--- postgresql/src/main/java/io/liski/postgresql/domain/Book.java
+++ postgresql/src/main/java/io/liski/postgresql/domain/Book.java
@@ -1,15 +1,91 @@
 package io.liski.postgresql.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Book {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long _id;
+    private Long id;
+    private boolean audio;
+    private String fragment;
+    private String isbn;
+    private String title;
 
+    @ManyToMany(mappedBy = "books")
+    private Set<Author> authors = new HashSet<>();
+
+    @ManyToMany(mappedBy = "books")
+    private Set<Kind> kinds = new HashSet<>();
+
+    @ManyToMany(mappedBy = "books")
+    private Set<Translator> translators = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isAudio() {
+        return audio;
+    }
+
+    public void setAudio(boolean audio) {
+        this.audio = audio;
+    }
+
+    public String getFragment() {
+        return fragment;
+    }
+
+    public void setFragment(String fragment) {
+        this.fragment = fragment;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public Set<Kind> getKinds() {
+        return kinds;
+    }
+
+    public void setKinds(Set<Kind> kinds) {
+        this.kinds = kinds;
+    }
+
+    public Set<Translator> getTranslators() {
+        return translators;
+    }
+
+    public void setTranslators(Set<Translator> translators) {
+        this.translators = translators;
+    }
 }
