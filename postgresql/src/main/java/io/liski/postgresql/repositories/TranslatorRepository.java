33a3262e64b9dc052d98f67c04216ce3cc10b0f3
Bartek <bartraw23@gmail.com>
2020-10-25 14:31:59 +0100

    Delete translator repository

--- postgresql/src/main/java/io/liski/postgresql/repositories/TranslatorRepository.java
+++
@@ -1,7 +0,0 @@
-package io.liski.postgresql.repositories;
-
-import io.liski.postgresql.domain.Translator;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TranslatorRepository extends CrudRepository<Translator, Long> {
-}
