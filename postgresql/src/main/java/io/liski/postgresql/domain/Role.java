94ef5ac2f269e310a4c6680c843db71919824d14
Bartek <bartraw23@gmail.com>
2020-10-26 12:03:26 +0100

    Add enum to roles

--- postgresql/src/main/java/io/liski/postgresql/domain/Role.java
+++ postgresql/src/main/java/io/liski/postgresql/domain/Role.java
@@ -12,24 +12,25 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Enumerated(EnumType.STRING)
+    private RoleName name;
 
     @OneToMany(mappedBy="role")
     Set<User> users = new HashSet<>();
 
-    public Long getRole_id() {
+    public Long getId() {
         return id;
     }
 
-    public void setRole_id(Long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getName() {
+    public RoleName getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(RoleName name) {
         this.name = name;
     }
 
---
+++ postgresql/src/main/java/io/liski/postgresql/domain/RoleName.java
@@ -0,0 +1,8 @@
+package io.liski.postgresql.domain;
+
+public enum RoleName {
+    SUPER_ADMIN,
+    ADMIN,
+    USER,
+    GUEST
+}
